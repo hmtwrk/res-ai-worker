35f845a46efdc553fdbe1b0a46a21e5414b7d29d
hmtwrk <hmtwrk@gmail.com>
2026-01-07 12:59:29 -0800

    Update model repository ID in builder.py

--- builder.py
+++ builder.py
@@ -3,6 +3,6 @@ from huggingface_hub import snapshot_download
 
 # This downloads the model to the folder /model inside the image
 snapshot_download(
-    repo_id="YOUR_MODEL_REPO_ID", 
+    repo_id="pillowcushion/res-ai", 
     local_dir="/model"
 )
